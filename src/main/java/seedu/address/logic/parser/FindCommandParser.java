82722234234fed8229ac2ecc409478f797ae257d
KJ-infosec <teykj0726@icloud.com>
2026-03-18 11:05:44 +0800

    Fix Bug in Parser

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -38,34 +38,46 @@ public class FindCommandParser implements Parser<FindCommand> {
 
         if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
             return new FindCommand(new PersonContainsKeywordsPredicate(
-                    argMultimap.getValue(PREFIX_NAME).get(),
+                    getNonEmptyValue(argMultimap, PREFIX_NAME),
                     PersonContainsKeywordsPredicate.SearchType.NAME));
         }
 
         if (argMultimap.getValue(PREFIX_PHONE).isPresent()) {
             return new FindCommand(new PersonContainsKeywordsPredicate(
-                    argMultimap.getValue(PREFIX_PHONE).get(),
+                    getNonEmptyValue(argMultimap, PREFIX_PHONE),
                     PersonContainsKeywordsPredicate.SearchType.PHONE));
         }
 
         if (argMultimap.getValue(PREFIX_EMAIL).isPresent()) {
             return new FindCommand(new PersonContainsKeywordsPredicate(
-                    argMultimap.getValue(PREFIX_EMAIL).get(),
+                    getNonEmptyValue(argMultimap, PREFIX_EMAIL),
                     PersonContainsKeywordsPredicate.SearchType.EMAIL));
         }
 
         if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
             return new FindCommand(new PersonContainsKeywordsPredicate(
-                    argMultimap.getValue(PREFIX_ADDRESS).get(),
+                    getNonEmptyValue(argMultimap, PREFIX_ADDRESS),
                     PersonContainsKeywordsPredicate.SearchType.ADDRESS));
         }
 
         if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
             return new FindCommand(new PersonContainsKeywordsPredicate(
-                    argMultimap.getValue(PREFIX_TAG).get(),
+                    getNonEmptyValue(argMultimap, PREFIX_TAG),
                     PersonContainsKeywordsPredicate.SearchType.TAG));
         }
         throw new ParseException(
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    /**
+     * Helper function to make sure the input value is non-empty.
+     */
+    private String getNonEmptyValue(ArgumentMultimap argMultimap, Prefix prefix) throws ParseException {
+        String value = argMultimap.getValue(prefix).get().trim();
+        if (value.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+        return value;
     }
 }
